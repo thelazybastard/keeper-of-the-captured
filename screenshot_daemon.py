a2569cb6905a2127e3582aaa13bf29cc25fc5fe8
Monish Giani <256585194+thelazybastard@users.noreply.github.com>
2026-02-03 00:31:49 +0700

    moves each image to their designated folder
    creates directories if missing

--- screenshot_daemon.py
+++ screenshot_daemon.py
@@ -2,8 +2,8 @@
 #  - use placeholder demo folder in pictures - done
 #  - extract data and convert to readable format - done
 #  - create list of keywords found in screenshots description - done
-#  - create folder names based on image description
-#  - create folders based on image description
+#  - create folder names based on image description - done
+#  - create folders based on image description - done
 
 from PIL import Image
 from transformers import CLIPProcessor, CLIPModel
@@ -12,7 +12,7 @@ import torch
 from image_keywords import all_keywords, keyword_to_folder
 import shutil
 
-screenshots = Path.home() / "Pictures/demo"
+pictures = Path.home() / "Pictures/demo"
 
 def load_models():
     model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
@@ -21,8 +21,9 @@ def load_models():
 
 
 def scan_main_directory():
-    for image in screenshots.iterdir():
+    for image in pictures.iterdir():
         yield image
+
 
 def load_image_contents():
     images = scan_main_directory()
@@ -36,13 +37,20 @@ def load_image_contents():
 
         with torch.no_grad():
             outputs = model(**inputs)
+            keyword_probability = outputs.logits_per_image.softmax(dim=1)[0]
+            predicted_keyword = labels[keyword_probability.argmax()]
+            folder_name = keyword_to_folder[predicted_keyword]
 
-        keyword_probability = outputs.logits_per_image.softmax(dim=1)[0]
-        predicted_keyword = labels[keyword_probability.argmax()]
-        folder_name = keyword_to_folder[predicted_keyword]
-        return folder_name
-
+            folder_path = pictures / folder_name
+            folder_path.mkdir(parents=True, exist_ok=True)
+            shutil.move(image, folder_path)
 
 load_image_contents()
+
+
+
+
+
+
 
 
