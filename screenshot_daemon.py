60f54b5808e048699f0937910cc2ee9f6cc91cf0
Monish Giani <256585194+thelazybastard@users.noreply.github.com>
2026-02-03 00:03:32 +0700

    creates folder names based on keywords

--- screenshot_daemon.py
+++ screenshot_daemon.py
@@ -1,18 +1,22 @@
 # TODO: image daemon classifying screenshots based on data inside.
 #  - use placeholder demo folder in pictures - done
 #  - extract data and convert to readable format - done
-#  - create list of description keywords found in screenshots
+#  - create list of keywords found in screenshots description - done
+#  - create folder names based on image description
+#  - create folders based on image description
 
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import CLIPProcessor, CLIPModel
 from pathlib import Path
 import torch
+from image_keywords import all_keywords, keyword_to_folder
+import shutil
 
 screenshots = Path.home() / "Pictures/demo"
 
 def load_models():
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
     return model, processor
 
 
@@ -23,19 +27,21 @@ def scan_main_directory():
 def load_image_contents():
     images = scan_main_directory()
     model, processor = load_models()
+    labels = all_keywords
 
     for image in images:
         each_image = Image.open(image)
 
-        inputs = processor(images=each_image, return_tensors="pt", padding=True)
+        inputs = processor(text=labels, images=each_image, return_tensors="pt", padding=True)
 
-        # generate image description
         with torch.no_grad():
-            generated_ids = model.generate(**inputs, max_new_tokens=40) # type: ignore
+            outputs = model(**inputs)
 
-        # convert to readable format
-        image_description = processor.decode(generated_ids[0], skip_special_tokens=True)
-        print("Description:", image_description)
+        keyword_probability = outputs.logits_per_image.softmax(dim=1)[0]
+        predicted_keyword = labels[keyword_probability.argmax()]
+        folder_name = keyword_to_folder[predicted_keyword]
+        return folder_name
+
 
 load_image_contents()
 
