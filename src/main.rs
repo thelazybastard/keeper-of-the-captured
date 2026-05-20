734d4823738d701f4a5bfcb01d51fdedc8e7f60b
thelazybastard <256585194+thelazybastard@users.noreply.github.com>
2026-05-20 17:51:10 +0700

    checks for path existence

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,54 @@
 use std::error::Error;
 use std::io::{self, Write};
+use std::{env, fs, path};
+use std::path::Path;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
+
     loop {
+
+        let home_dir: String = match env::var("HOME") {
+            Ok(e) => e,
+            Err(_) => return
+        };
+
         print!("Enter Directory to clean: ");
-        io::stdout().flush()?;
+        match io::stdout().flush() {
+            Ok(_) => (),
+            Err(_) => continue 
+        };
+
         let mut user_input: String = String::new();
         match io::stdin().read_line(&mut user_input) {
-            Ok(bytes_read) => {
-                break
-            }
-            Err(error) => {
-                continue
-            }
+            Ok(_) => (),
+            Err(_) => continue         
         } 
+        user_input = user_input.trim().to_string();
+
+        let path_to_iter = Path::new(&home_dir);
+        path_to_iter.join(&user_input);
+        
+        iterate_directory(&path_to_iter);
+    }
+}
+
+fn iterate_directory(path_to_iter: &Path) {
+
+    let check_path = Path::new(path_to_iter);
+    if !check_path.exists() && !check_path.is_dir() {
+        return
     }
 
-    Ok(())
+    let image_extensions: [&str; 5] = [".jpg", ".jpeg", ".png", ".bmp", ".webp"];
+
+    let entries = match fs::read_dir(path_to_iter) {
+        Ok(e) => e,
+        Err(_) => return
+    };
+    for entry in entries {
+        
+    }
 }
+
 
 
